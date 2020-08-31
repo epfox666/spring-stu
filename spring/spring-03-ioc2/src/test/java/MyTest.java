afddaedfc31755af267e5ca4f08cda61b2fc0711
username <user@mail.com>
2020-08-31 22:20:00 +0800

    spring-stu spring4

--- spring/spring-03-ioc2/src/test/java/MyTest.java
+++ spring/spring-03-ioc2/src/test/java/MyTest.java
@@ -1,4 +1,5 @@
 import com.epfox.pojo.User;
+import com.epfox.pojo.UserT;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -16,5 +17,23 @@ public class MyTest {
         User user = (User) context.getBean("user");
         User user2 = (User) context.getBean("user");
         System.out.println(user==user2);
+    }
+    @Test
+    public void test2() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        User user = (User) context.getBean("userNew");
+        user.show();
+    }
+    @Test
+    public void test3() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        UserT user = (UserT) context.getBean("u4");
+        user.show();
+    }
+    @Test
+    public void test4() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        UserT user = (UserT) context.getBean("u5");
+        user.show();
     }
 }
---
+++ spring/spring-04-di/src/test/java/MyTest.java
@@ -0,0 +1,26 @@
+import com.epfox.pojo.Student;
+import com.epfox.pojo.User;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MyTest {
+    @Test
+    public void test(){
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        Student student = (Student) context.getBean("student");
+        System.out.println(student);
+    }
+    @Test
+    public void test2(){
+        ApplicationContext context = new ClassPathXmlApplicationContext("userbeans.xml");
+        User user = context.getBean("user", User.class);
+        System.out.println(user);
+    }
+    @Test
+    public void test3(){
+        ApplicationContext context = new ClassPathXmlApplicationContext("userbeans.xml");
+        User user = context.getBean("user2", User.class);
+        System.out.println(user);
+    }
+}
