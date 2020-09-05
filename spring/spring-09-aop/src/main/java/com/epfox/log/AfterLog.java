96e88afd9b1eebd1d276a7ed0ff6d4a01701968d
username <user@mail.com>
2020-09-05 10:08:05 +0800

    spring-stu spring-final

--- spring/spring-09-aop/src/main/java/com/epfox/log/AfterLog.java
+++ spring/spring-09-aop/src/main/java/com/epfox/log/AfterLog.java
@@ -6,7 +6,10 @@ import java.lang.reflect.Method;
 
 public class AfterLog implements AfterReturningAdvice {
 
-    //returnValue: 返回值
+    //returnValue 返回值
+    //method被调用的方法
+    //args 被调用的方法的对象的参数
+    //target 被调用的目标对象
     public void afterReturning(Object returnValue, Method method, Object[] args, Object target) throws Throwable {
         System.out.println("执行了"+method.getName()+"方法，返回结果为:" + returnValue);
     }
--- spring/spring-09-aop/src/main/java/com/epfox/log/Log.java
+++ spring/spring-09-aop/src/main/java/com/epfox/log/Log.java
@@ -5,8 +5,8 @@ import org.springframework.aop.MethodBeforeAdvice;
 import java.lang.reflect.Method;
 
 public class Log implements MethodBeforeAdvice {
-    //method: 要执行的目标参数
-    //args:参数
+    //method: 要执行的目标对象的方法
+    //args:被调用的方法的参数
     //target:目标对象
     public void before(Method method, Object[] args, Object target) throws Throwable {
         System.out.println(target.getClass().getName()+"的"+method.getName()+"被执行了");
